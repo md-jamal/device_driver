6f6c04869981e45a40ae9d9d7cd8b54c84e8c6c0
Mohammad Jamal <md.jamalmohiuddin@gmail.com>
2017-11-12 07:48:24 +0530

    First Day Hello World Test Programs

---
+++ HelloWorld/driver_3.c
@@ -0,0 +1,18 @@
+#include <linux/init.h>
+#include <linux/module.h>
+
+MODULE_LICENSE("GPL");
+
+int init_module(void)
+{
+	printk(KERN_ALERT"Hello World\n");
+	return 0;
+}
+
+void cleanup_module(void)
+{
+	printk(KERN_ALERT"Hello exit\n");
+}
+
+//module_init(hello_init);
+//module_exit(hello_exit);
