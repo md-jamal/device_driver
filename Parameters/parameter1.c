80e9c67b45ecf9c05ee5a98184626336ff2a968e
Mohammad Jamal <md.jamalmohiuddin@gmail.com>
2017-11-12 17:36:47 +0530

    Module Parameters Sample Code

---
+++ Parameters/parameter1.c
@@ -0,0 +1,33 @@
+#include <linux/init.h>
+#include <linux/module.h>
+#include <linux/moduleparam.h>
+#include <linux/stat.h>
+
+MODULE_LICENSE("GPL");
+
+static char *whom = "world";
+static int howmany = 1;
+
+module_param(howmany, int, S_IRUGO);
+module_param(whom, charp, S_IRUGO);
+
+static int __init hello_init(void)
+{
+	printk(KERN_ALERT"Hello INIT\n");
+	while(howmany > 0 )
+	{
+		printk("%s\n", whom);
+		howmany--;
+	}
+	return 0;
+}
+
+static void __exit hello_exit(void)
+{
+	printk(KERN_ALERT"Hello EXIT\n");
+
+}
+
+
+module_init(hello_init)
+module_exit(hello_exit)
